28788fb9d004c5082b153ccfc5c1df6560953b9e
Arosh59 <aroshdanza59@gmail.com>
2025-11-03 02:08:20 -0800

    Implement login and registration pages with routing; add styles and background image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
-const Home: React.FC = () => {
-  return <h1>Sample Home Page</h1>;
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Welcome to Home Page!</h1>
+      <Link to="/login">
+        <button style={{ marginTop: "20px", padding: "10px 20px" }}>Go to Login</button>
+      </Link>
+
+       <Link to="/register">
+        <button style={{ marginTop: "20px", padding: "10px 20px" }}>Go to Register</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/pages/Register.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import "../styles/Register.css";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <div className="register-container">
+      <div className="overlay"></div>
+      <form className="register-glass" onSubmit={handleSubmit}>
+        <h2>Create Account </h2>
+
+        <input
+          type="text"
+          placeholder="Full Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+
+        <input
+          type="email"
+          placeholder="Email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
