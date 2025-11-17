47aa4aa6100bf4c7f3dd00eeab66de484c604300
Arosh59 <aroshdanza59@gmail.com>
2025-11-16 21:42:59 -0800

    Load Tailwind CSS dynamically before rendering the React application

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const tw = document.createElement("script");
+tw.src = "https://cdn.tailwindcss.com";
+
+tw.onload = () => {
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
+
+document.head.appendChild(tw);
